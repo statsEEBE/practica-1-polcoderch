3dc33d36aeec2c94470ba57ee5b1af344bfdfdbb
polcoderch <pol.coderch.rodriguez@estudiantat.upc.edu>
2024-09-19 13:22:10 +0200

    .

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,19 @@
 #Codigo para problema 1
+mis_dades <- mtcars
+qseq_intervals <- cut(mis_dades$qsec,4)
+ni <- table(qseq_intervals)
+fri <- ni/sum(ni)
+Ni <-cumsum(ni)
+Fi <- Ni/sum(ni)
+cbind(ni,fri,Ni,Fi)
 
-plot(1:5)
+hist(mis_dades$qsec)
+mean(mis_dades$qsec)
+
+sort(mis_dades$draft)
+median(mis_dades$draft)
+
+quantile(mis_dades$draft,0.25)
+quantile(mis_dades$draft,0.75)
+
+quantile(mis_dades$d,0.75)
